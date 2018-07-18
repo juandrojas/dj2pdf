fab0f4f02aa949ce4e7218cf037601c5f9245564
Juan Diego Rojas <juandrojas@192.168.0.5>
2018-07-17 20:24:47 -0500

    fixed some bugs regarding files extension, added function enviroment in script

--- dj2pdf.sh
+++ dj2pdf.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-FILE=$1
-OUTPUT=$2
-djvu2pdf $FILE
-echo "${FILE%%.*}.pdf"
-djvused "${FILE}" -e 'print-outline' > bmarks.out
-pdftk "${FILE%%.*}.pdf" dump_data > pdfmetadata.out
-python bmarkDjvu2pdf.py
-sed '/^NumberOfPages: / r bmarks.txt' pdfmetadata.out > pdfmetadata.in
-pdftk "${FILE%%.*}.pdf" update_info "pdfmetadata.in" output "${OUTPUT}"
-rm pdfmetadata.in
-rm pdfmetadata.out
-rm bmarks.out
-rm bmarks.txt
+function dj2pdf() {
+  FILE=$1
+  OUTPUT=$2
+  djvu2pdf $FILE
+  echo "${FILE%%.*}.pdf"
+  mv "${FILE%%.*}.pdf" test.pdf
+  djvused "${FILE}" -e 'print-outline' > bmarks.out
+  pdftk test.pdf dump_data > pdfmetadata.out
+  python bmarkDjvu2pdf.py
+  sed '/^NumberOfPages: / r bmarks.txt' pdfmetadata.out > pdfmetadata.in
+  pdftk test.pdf update_info "pdfmetadata.in" output "${OUTPUT}"
+  rm test.pdf
+  rm pdfmetadata.in
+  rm pdfmetadata.out
+  rm bmarks.out
+  rm bmarks.txt
+}
